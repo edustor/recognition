8aefc70f04f4209b0e6ffd4bdd4e66d7aa87a731
Dmitry Romanov <me@wutiarn.ru>
2016-11-27 13:24:24 +0300

    Rabbitmq setup

---
+++ src/main/kotlin/ru/edustor/recognition/config/RabbitConfig.kt
@@ -0,0 +1,25 @@
+package ru.edustor.recognition.config
+
+import org.springframework.amqp.core.Binding
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class RabbitConfig {
+    @Bean
+    open fun rabbitRejectedExchange(): TopicExchange {
+        return TopicExchange("reject.edustor.ru", true, false)
+    }
+
+    @Bean
+    open fun rabbitRejectedQueue(): Queue {
+        return Queue("rejected.edustor.ru", true, false, false)
+    }
+
+    @Bean
+    open fun rebbitRejectedBinding(): Binding {
+        return Binding("rejected.edustor.ru", Binding.DestinationType.QUEUE, "reject.edustor.ru", "#", null)
+    }
+}
--- src/main/kotlin/ru/edustor/recognition/rabbit/RabbitHandler.kt
+++ src/main/kotlin/ru/edustor/recognition/rabbit/RabbitHandler.kt
@@ -1,7 +1,21 @@
 package ru.edustor.recognition.rabbit
 
-class RabbitHandler {
-    fun processFile() {
-        
+import org.springframework.amqp.core.ExchangeTypes
+import org.springframework.amqp.rabbit.annotation.*
+import org.springframework.stereotype.Component
+
+@Component
+open class RabbitHandler {
+    @RabbitListener(bindings = arrayOf(QueueBinding(
+            value = Queue("recognition.edustor.ru/incoming", durable = "true", arguments = arrayOf(
+                    Argument(name = "x-dead-letter-exchange", value = "reject.edustor.ru")
+            )),
+            exchange = Exchange("internal.edustor.ru", type = ExchangeTypes.TOPIC,
+                    ignoreDeclarationExceptions = "true",
+                    durable = "true"),
+            key = "uploaded.pdf.events"
+    )))
+    fun processFile(msg: ByteArray) {
+
     }
 }
