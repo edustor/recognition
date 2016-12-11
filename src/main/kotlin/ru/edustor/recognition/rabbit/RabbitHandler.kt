d7fc5b4e0fba39d8d3b9e54c0ecd40abbd9262d8
Dmitry Romanov <me@wutiarn.ru>
2016-12-11 09:32:09 +0300

    Switch from protobuf to json

--- src/main/kotlin/ru/edustor/recognition/rabbit/RabbitHandler.kt
+++ src/main/kotlin/ru/edustor/recognition/rabbit/RabbitHandler.kt
@@ -1,16 +1,14 @@
 package ru.edustor.recognition.rabbit
 
-import com.google.protobuf.InvalidProtocolBufferException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.amqp.AmqpRejectAndDontRequeueException
 import org.springframework.amqp.core.ExchangeTypes
 import org.springframework.amqp.rabbit.annotation.*
 import org.springframework.amqp.rabbit.annotation.Queue
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Component
-import ru.edustor.commons.protobuf.proto.internal.EdustorPdfProcessingProtos.PageRecognizedEvent
-import ru.edustor.commons.protobuf.proto.internal.EdustorPdfProcessingProtos.PdfUploadedEvent
+import ru.edustor.commons.models.internal.processing.pdf.PageRecognizedEvent
+import ru.edustor.commons.models.internal.processing.pdf.PdfUploadedEvent
 import ru.edustor.commons.storage.service.BinaryObjectStorageService
 import ru.edustor.commons.storage.service.BinaryObjectStorageService.ObjectType
 import ru.edustor.recognition.exception.PdfNotFoundException
@@ -33,14 +31,7 @@ open class RabbitHandler(var storage: BinaryObjectStorageService, val rabbitTemp
                     durable = "true"),
             key = "uploaded.pdf.pages.processing"
     )))
-    fun processFile(msg: ByteArray) {
-        val event: PdfUploadedEvent
-        try {
-            event = PdfUploadedEvent.parseFrom(msg)
-        } catch (e: InvalidProtocolBufferException) {
-            throw AmqpRejectAndDontRequeueException(e)
-        }
-
+    fun processFile(event: PdfUploadedEvent) {
         logger.info("Processing file ${event.uuid} uploaded by ${event.userId}")
 
         val uploadedPdfStream = storage.get(ObjectType.PDF_UPLOAD, event.uuid)
@@ -62,15 +53,14 @@ open class RabbitHandler(var storage: BinaryObjectStorageService, val rabbitTemp
 
             storage.put(ObjectType.PAGE, pageUuid, pageBytes.inputStream(), pageBytes.size.toLong())
 
-            val recognizedEvent = PageRecognizedEvent.newBuilder()
-                    .setUploadUuid(event.uuid)
-                    .setPageIndex(i++)
-                    .setPageUuid(pageUuid)
-                    .setQrUuid(qrUuid)
-                    .setUserId(event.userId)
-                    .setTargetLessonId(event.targetLessonId)
-                    .build()
-            rabbitTemplate.convertAndSend("internal.edustor", "recognized.pages.processing", recognizedEvent.toByteArray())
+            val recognizedEvent = PageRecognizedEvent(event.uuid,
+                    pageUuid,
+                    event.userId,
+                    i++,
+                    qrUuid,
+                    event.targetLessonId,
+                    event.timestamp)
+            rabbitTemplate.convertAndSend("internal.edustor", "recognized.pages.processing", recognizedEvent)
 
             logger.info("Processed page $i ($pageUuid). QR: $qrData")
         }
